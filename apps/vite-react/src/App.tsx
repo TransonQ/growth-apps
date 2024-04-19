761b810defec73a4adff9bef9b79345aef406116
TransonQ <quanscheng@qq.com>
2024-04-19 23:26:51 +0800

    setup: ui-add

--- apps/vite-react/src/App.tsx
+++ apps/vite-react/src/App.tsx
@@ -1,9 +1,29 @@
 import { Button } from "@mono/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@mono/ui/card"
 
 function App() {
   return (
     <>
       <Button>click</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </>
   )
 }
