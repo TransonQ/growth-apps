dc4ee03c179899c83ee7e2f61d7202993c325776
TransonQ <quanscheng@qq.com>
2024-04-20 18:09:57 +0800

    build: Dividing packages by the first letter

--- apps/vite-react/vite.config.ts
+++ apps/vite-react/vite.config.ts
@@ -10,4 +10,27 @@ export default defineConfig({
       "~": resolve(__dirname, "src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          // if (id.includes("node_modules")) {
+          //   return (
+          //     "vendor-" +
+          //     id.toString().split("node_modules/")[1].split("/")[0].toString()
+          //   )
+          // }
+          if (id.includes("node_modules")) {
+            const packageName = id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString()
+            const firstLetter = packageName[0].toLowerCase()
+            return "vendor-" + firstLetter // Dividing packages by the first letter.
+          }
+        },
+      },
+    },
+  },
 })
